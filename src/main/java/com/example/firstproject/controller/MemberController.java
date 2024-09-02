7d92faa1c05e7ad57d497c244873893465eafda1
SO YEON LEE <thdus1323@gmail.com>
2024-09-02 22:27:08 +0900

    실습-삭제하기

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -87,5 +88,19 @@ public class MemberController {
             memberRepository.save(member);
         }
         return "redirect:/members/" + member.getId();
+    }
+
+    //삭제하기
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+//        1)삭제할 대상 가져오기
+Member target = memberRepository.findById(id).orElse(null);
+//        2)대상 엔티티 삭제하기
+if ( target != null){
+    memberRepository.delete(target);
+    rttr.addFlashAttribute("msg", "삭제완료!");
+}
+//        3)결과 페이지로 리다이렉트
+        return "redirect:/members";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
 insert into article(id, title, content) values (1, '가가가가', '1111');
 insert into article(id, title, content) values (2, '나나나나', '2222');
 insert into article(id, title, content) values (3, '다다다다', '3333');
+
+
+--------------------------------------------------------------
+--회원 --
+insert into member(id, email, password) values (1, 'ssar@nate.com', '1111');
+insert into member(id, email, password) values (2, 'cos@nate.com', '2222');
+insert into member(id, email, password) values (3, 'love@nate.com', '3333');
